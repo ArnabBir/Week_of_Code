903d63c4c20172de51fbc3b3d7b1b6ad3f274820
Arnab Bir <arnabbir@gmail.com>
2016-10-14 23:20:37 +0530

    Update Happy_Ladybugs.cpp

--- Happy_Ladybugs.cpp
+++ Happy_Ladybugs.cpp
@@ -19,7 +19,6 @@ int main(){
         int _Count = count(b, b + n, '_');
         if(_Count == n) cout<<"YES"<<endl;
         else{
-            
             vector<int> colCount(26);
             int color = 0;
             for(vector<int>::iterator itr = colCount.begin(); itr != colCount.end(); ++itr){
@@ -41,6 +40,8 @@ int main(){
                 cout<<"YES"<<endl;
                 continue;
             }
+            if(b[0] != b[1] && b[0]!= '_' && b[1]!= '_') flag = true;
+            if(b[n-2] != b[n-1] && b[n-2]!= '_' && b[n-1]!= '_') flag = true;
             for(int pos = 1; pos < n - 1; pos += 2){
                 if(b[pos] != '_')
                     if(b[pos] != b[pos-1] && b[pos] != b[pos+1]){
